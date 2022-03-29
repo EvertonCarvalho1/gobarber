99cb588c26d0ae9a4b5f77923bfe947f84910c98
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2022-03-29 11:21:05 -0300

    building a toast hook

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import SignUp from './pages/SignUp';
 import ToastContainer from './components/ToastContainer';
 import GlobalStyle from './styles/global';
 
-import { AuthProvider } from './hooks/AuthContext';
+import { AuthProvider } from '../src/hooks/auth';
+import { ToastProvider } from '../src/hooks/toast';
 //AuthContext.Provider é um componente que colocamos por volta dos componentes que queremos que tenham o contexto de autenticação.
 
 // Todos os componentes dentro do contexto, terão acessos as informações do contexto, até aqueles componentes dentro dos componentes.
