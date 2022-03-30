55b2c885b3cc9755f5b786b7716f07f6bb4e6e26
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2022-03-30 10:47:34 -0300

    timing toast

--- src/components/ToastContainer/index.tsx
+++ src/components/ToastContainer/index.tsx
@@ -1,17 +1,13 @@
-import React, {useCallback} from "react";
-import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
-
-import { ToastMessage, useToast } from '../../hooks/toast'
-import { Container, Toast } from "./styles";
+import React from "react";
+import { ToastMessage } from '../../hooks/toast'
+import { Container } from "./styles";
+import Toast from "./Toast";
 
 interface ToastContainerProps {
     messages: ToastMessage[];
 }
 
 const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
-
-    const { removeToast } = useToast(); 
-     
     return (
         <Container>
             {messages.map(message => {
@@ -19,20 +15,10 @@ const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
                 //não podemos enviar um parâmetro dentro de uma função que esta no onchange por exemplo, se não codigo executa, devemos colocar a função dentro de uma arrow function e assim passar o parametro
                 return (
                     <Toast
-                        type={message.type}
                         key={message.id}
-                        hasDescription={!!message.description}
-                    >
-                        <FiAlertCircle size={20} />
-                        <div>
-                            <strong>{message.title}</strong>
-                            {message.description && <p>{message.description}</p>}
-                        </div>
-
-                        <button onClick={() => removeToast(message.id)} type="button">
-                            <FiXCircle size={18} />
-                        </button>
-                    </Toast>)
+                        toast={message}
+                    />
+                )
             })}
         </Container>
     )
