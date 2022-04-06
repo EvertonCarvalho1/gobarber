5eb2f2efc03930d2dd8a92e69786e8a67ec59d11
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2022-04-06 20:39:19 -0300

    creating recovery page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,6 +7,7 @@ import Route from "./Route";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
 import Dashboard from "../pages/Dashboard";
+import ForgotPassword from "../pages/ForgotPassword";
 
 
 const Routes: React.FC = () => {
@@ -15,6 +16,7 @@ const Routes: React.FC = () => {
             <Route path='/' exact component={SignIn} />
             <Route path='/signup' exact component={SignUp} />
             <Route path='/dashboard' component={Dashboard} isPrivate/>
+            <Route path='/forgotpassword' component={ForgotPassword}/>
         </Switch>
     )
 };
